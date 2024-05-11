0c495be2cda5061976d54c4bc3e163db97fd4ab5
paterson <matus.petrovaj@gmail.com>
2024-05-11 18:33:19 +0200

    Refactor user manual

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -5,7 +5,9 @@
         </h1>
         <form @submit.prevent="showQuestion" class="space-y-4">
             <div>
-                <label for="question" class="block text-sm font-medium text-gray-700">{{ __('Enter a question ID') }}:</label>
+                <label for="question" class="block text-sm font-medium text-gray-700">{{
+                        __('Enter a question ID')
+                    }}:</label>
                 <input type="text" id="question" v-model="form.question"
                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                        placeholder="ID is a 5 letter code">
@@ -15,44 +17,16 @@
                 {{ __('Show Question') }}
             </button>
         </form>
-        <button @click="isModalOpen = true"
-                class="mt-8 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
-            {{ __('Open User Manual') }}
-        </button>
-        <!-- Modal Section -->
-        <transition name="fade">
-            <div v-if="isModalOpen" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-                <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-                    <div class="mt-3 text-center">
-                        <h3 class="text-lg leading-6 font-medium text-gray-900">{{ __('User Manual') }}</h3>
-                        <div class="mt-2 px-7 py-3">
-                            <p class="text-sm text-gray-500">
-                                Here you can provide instructions or any other information needed. Explain how to use
-                                the application, provide FAQ, etc.
-                            </p>
-                        </div>
-                        <div class="items-center px-4 py-3">
-                            <button @click="isModalOpen = false"
-                                    class="px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300">
-                                {{ __('Close') }}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </transition>
     </div>
 </template>
 
 <script setup>
-import {ref} from 'vue';
 import {router, useForm} from '@inertiajs/vue3'
 import {route} from "ziggy-js";
 
 const form = useForm({
     question: null,
 })
-const isModalOpen = ref(false);
 
 const showQuestion = () => router.get(route('question.show', form.question))
 </script>
--- resources/js/Pages/Layouts/MainLayout.vue
+++ resources/js/Pages/Layouts/MainLayout.vue
@@ -6,7 +6,7 @@
                 <h1 class="text-xl font-bold">{{ __('Voting System') }}</h1>
 
                 <div class="flex space-x-4 items-center">
-                    <language-selector class="order-last ml-6"/>
+                    <language-selector class="order-last ml-4"/>
 
                     <nav>
                         <ul class="flex space-x-4">
@@ -69,6 +69,73 @@
                             </li>
                         </ul>
                     </nav>
+
+                    <button @click="isModalOpen = true"
+                            class="py-2 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                        {{ __('Manual') }}
+                    </button>
+                    <!-- Modal Section -->
+                    <!-- Modal Section -->
+                    <transition name="fade">
+                        <div v-if="isModalOpen"
+                             class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+                            <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+                                <div class="mt-3">
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900 text-center">
+                                        {{ __('User Manual') }}</h3>
+                                    <div class="mt-2 px-7 py-3 text-sm text-gray-500 pdf-content">
+                                        <p>
+                                            Here is a <b>manual</b> for the page. Firstly we will start with roles and
+                                            their
+                                            permissions.
+                                        </p>
+                                        <ul class="list-disc list-inside">
+                                            <li>
+                                                <b>Unlogged user</b>: join voting for specific question via code/QR,
+                                                view voting results
+
+                                            </li>
+                                            <li>
+                                                <b>Logged user</b>: same as unlogged user, define your own voting
+                                                questions,
+                                                change password, activate/deactivate your questions, edit/delete/copy
+                                                your existing questions,
+                                                define questions subject, filter questions based on subject and date of
+                                                creation, view also archived questions, export questions and answers to
+                                                JSON
+                                            </li>
+                                            <li>
+                                                <b>Admin</b>: same as logged user, ability to view all questions and act
+                                                on behalf of all other users, CRUD operations regarding all users
+                                            </li>
+                                        </ul>
+                                        <p>
+                                            <b>The homepage</b> consists of form where you can insert 5-letter code/scan
+                                            QR code to join voting, but also edit/delete/duplicate/deactivate question,
+                                            close voting, etc..
+                                            In the top right corner, you will see <B>menu, manual and language
+                                            selector</B>. From the menu you have multiple options like displaying
+                                            dashboard, creating questions, managing account, etc.. Menu items depend on
+                                            the role of specific user.
+                                        </p>
+                                    </div>
+
+                                    <div class="items-center px-4 py-3">
+                                        <button @click="exportToPDF"
+                                                class="mb-2 px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                                            {{ __('Export to PDF') }}
+                                        </button>
+                                        <button @click="isModalOpen = false"
+                                                class="px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300">
+                                            {{ __('Close') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </transition>
+
+
                 </div>
             </div>
         </header>
@@ -87,13 +154,29 @@
 
 
 <script setup>
-import {Link, usePage} from "@inertiajs/vue3";
-import {route} from "ziggy-js";
+import {ref} from 'vue';
+import {jsPDF} from "jspdf";
 import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
 import {ChevronDownIcon} from '@heroicons/vue/20/solid'
 import LanguageSelector from "../Components/LanguageSelector.vue";
+import {Link, usePage} from "@inertiajs/vue3";
+import {route} from "ziggy-js";
 
 const page = usePage();
+const isModalOpen = ref(false);
+
+function exportToPDF() {
+    const content = document.querySelector('.pdf-content').innerText;
+    const doc = new jsPDF({
+        orientation: 'portrait',
+        unit: 'pt',
+        format: 'a4'
+    });
+
+    const lines = doc.splitTextToSize(content, 500); // Split long text into lines that fit the page width
+    doc.text(lines, 40, 40); // Start the text 40 points from the top and 40 from the left margin
+    doc.save('user-manual.pdf');
+}
 </script>
 
 <style scoped>
